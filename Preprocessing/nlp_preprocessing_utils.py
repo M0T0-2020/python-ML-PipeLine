88f1920510683e6e7f8f369ef1a2a7e8d7de6b38
M0T0 <abcdmoto0509@outlook.jp>
2022-03-03 21:00:22 +0900

    add nlp process

--- Preprocessing/nlp_preprocessing_utils.py
+++ Preprocessing/nlp_preprocessing_utils.py
@@ -19,10 +19,18 @@ from googletrans import Translator
 
 import re
 import textstat
-from nltk.corpus import stopwords, wordnet
-import nltk, string
+import string, re
+
+import nltk
+from nltk.corpus import wordnet
 from nltk.stem.porter import PorterStemmer
 from nltk import WordNetLemmatizer
+from nltk import word_tokenize
+from nltk.stem import SnowballStemmer
+from nltk.corpus import stopwords as nltk_stopwords
+
+import emoji
+from janome.tokenizer import Tokenizer
 
 #単語の定義を取ってくる
 def get_definition(s):
@@ -86,9 +94,12 @@ class GoogleTranslate:
 
 # Bertの埋め込み表現
 class BertSequenceVectorizer:
-    def __init__(self):
+    def __init__(self, model_name='bert-base-uncased'):
+        """
+        cl-tohoku/bert-base-japanese-whole-word-masking
+        """
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        self.model_name = 'bert-base-uncased'
+        self.model_name = model_name
         self.tokenizer = transformers.BertTokenizer.from_pretrained(self.model_name)
         self.bert_model = transformers.BertModel.from_pretrained(self.model_name)
         self.bert_model = self.bert_model.to(self.device)
@@ -162,24 +173,110 @@ class LanguageDetectFeatureExtractor(BaseTransformer):
             X[f"{col}_languagefeature"] = X[col].fillna("").map(lambda x: self.language_detect(x))
         return X
 
+#日本語の分かち書き
+def get_wakati_ja(s):
+    """
+    ==usagge==
+    df["ja_new_text"] = df["ja_text"].apply(get_wakati_ja)
+    """
+    tknz = Tokenizer()
+
+    UNICODE_EMOJI = []
+    for lang, value in emoji.UNICODE_EMOJI.items():
+        UNICODE_EMOJI += list(value.keys())
+    UNICODE_EMOJI = list(set(UNICODE_EMOJI))
+
+    l = []
+    speech = ["名詞", "動詞", "形容詞" ]
+    code_regex = re.compile('[!"#$%&\'\\\\()*+,-./:;<=>?@[\\]^_`{|}~「」〔〕“”〈〉『』【】＆＊・（）＄＃＠。、？！｀＋￥％]')
+    s = code_regex.sub(' ', s)
+    for token in tknz.tokenize(s):
+        if token.part_of_speech.split(",")[0] in speech:
+            t = token.base_form
+            t = t.replace(' ', '')
+            if len(t)==0:
+                continue
+            if len(t)==1:
+                if len(re.findall('[\u3041-\u309F]+', t))>0:
+                    continue
+            if t in UNICODE_EMOJI:
+                continue
+            #t = token.surface
+            l.append(t)
+    l = [s.replace(' ', '') for s in l]
+    l = [s for s in l if len(s)>0]
+    s = " ".join(l)
+    return s
+
+def change_text_en(text):
+    """
+    ==usagge==
+    df["english_new_text"] = df["english_text"].apply(change_text_en)
+    """
+    stop_words = list(nltk_stopwords.words("english"))+[' '*i for i in range(10)]
+    porter = PorterStemmer()
+    lemma = WordNetLemmatizer()  # NOTE: 複数形の単語を単数形に変換する
+    #usage
+    #train['text'] = train['text'].parallel_apply(self.change_text)
+    text = re.sub("[^a-zA-Z]", " ", text).lower()
+    text = nltk.word_tokenize(text)  # NOTE: 英文を単語分割する
+    text = [word for word in text if not word in stop_words]
+    text = [porter.stem(word) for word in text]
+    text = [s.replace(' ', '') for s in text]
+    text = [s for s in text if len(s)>0]
+    text =  " ".join(text)
+    #text =  " ".join([lemma.lemmatize(word) for word in text])
+    return text
+
+class change_text:
+    """
+    ==usagge==
+    change_fr_text = change_text('fr')
+    df["new_text"] = df["text"].apply(change_fr_text)
+    """
+    
+    @staticmethod
+    def get_language():
+        print(nltk_stopwords.fileids())
+        print(list(SnowballStemmer.languages))
+        
+    def __init__(self, language="english", stem=False):
+        assert language in nltk_stopwords.fileids() and language in list(SnowballStemmer.languages), "language is not in stopwords fileids or SnowballStemmer languages"
+        
+        UNICODE_EMOJI = []
+        for lang, value in emoji.UNICODE_EMOJI.items():
+            UNICODE_EMOJI += list(value.keys())
+        UNICODE_EMOJI = list(set(UNICODE_EMOJI))
+
+        self. code_regex = re.compile('[!"#$%&\'\\\\()*+,-./:;<=>?@[\\]^_`{|}~「」〔〕“”〈〉『』【】＆＊・（）＄＃＠。、？！｀＋￥％]')
+        self.stemmer = SnowballStemmer(language)
+        self.stopwords = [" "*i for i in range(10)] + nltk_stopwords.words(language)+ UNICODE_EMOJI
+        self.stem = stem
+    
+    def set_stem(self, stem):
+        self.stem = stem
+        
+    def add_stopwords(self, add_words):
+        if type(add_words)==str:
+            add_words = [add_words]
+        self.stopwords += add_words
+        
+    def __call__(self, text):
+        text = text.lower()
+        text = self.code_regex.sub(' ', text)
+        text = word_tokenize(text)
+        if self.stem:
+            text = [self.stemmer.stem(token) for token in text]
+        text = [word for word in text if not word in self.stopwords]
+        text = [s.replace(' ', '') for s in text]
+        text = [s for s in text if len(s)>0]
+        text =  " ".join(text)
+        return text
+    
 class TfidfVectorFeatureExtractor(BaseTransformer):
     def __init__(self, col):
-        stop_words = list(stopwords.words("english"))
-        self.stop_words = stop_words+[' ', '  ','']
-        self.porter = PorterStemmer()
-        self.lemma = WordNetLemmatizer()  # NOTE: 複数形の単語を単数形に変換する
         self.vec_tfidf = TfidfVectorizer()
         self.col = col
-        
-    def change_text(self, text):
-        #usage
-        #train['text'] = train['text'].parallel_apply(self.change_text)
-        text = re.sub("[^a-zA-Z]", " ", text).lower()
-        text = nltk.word_tokenize(text)  # NOTE: 英文を単語分割する
-        text = [word for word in text if not word in self.stop_words]
-        text =  " ".join([self.porter.stem(word) for word in text])
-        #text =  " ".join([lemma.lemmatize(word) for word in text])
-        return text
     
     def fit(self, df):
         self.vec_tfidf.fit(df[self.col].values)
@@ -192,27 +289,19 @@ class TfidfVectorFeatureExtractor(BaseTransformer):
 
 class CountVectorFeatureExtractor(BaseTransformer):
     def __init__(self, col):
-        stop_words = list(stopwords.words("english"))
-        self.stop_words = stop_words+[' ', '  ','']
-        self.porter = PorterStemmer()
-        self.lemma = WordNetLemmatizer()  # NOTE: 複数形の単語を単数形に変換する
         self.vec_count = CountVectorizer()
         self.col = col
         
-    def change_text(self, text):
-        #usage
-        #train['text'] = train['text'].parallel_apply(self.change_text)
-        text = re.sub("[^a-zA-Z]", " ", text).lower()
-        text = nltk.word_tokenize(text)  # NOTE: 英文を単語分割する
-        text = [word for word in text if not word in self.stop_words]
-        text =  " ".join([self.porter.stem(word) for word in text])
-        #text =  " ".join([lemma.lemmatize(word) for word in text])
-        return text
-
     def fit(self, df):
         self.vec_count.fit(df[self.col].values)
         
     def transform(self, df):
         X = self.vec_count.transform(df[self.col].values)
-        X = pd.DataFrame(X.toarray(), columns=self.vec_count.get_feature_names())
+        X = pd.DataFrame(X.toarray(), columns=self.vec_count.get_feature_names_out())
+        return X
+
+    def fit_transform(self, df, y=None, **fit_params):
+        self.vec_count.fit(df[self.col].values)
+        X = self.vec_count.transform(df[self.col].values)
+        X = pd.DataFrame(X.toarray(), columns=self.vec_count.get_feature_names_out())
         return X
